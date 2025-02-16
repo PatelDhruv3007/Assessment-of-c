07e33a03205c5c56ba0542f9181d691cf03fbe86
PatelDhruv3007 <pateldv0704@gmail.com>
2025-02-16 18:32:49 +0530

    Assesment.c

--- Assesment.c
+++ Assesment.c
@@ -1,135 +1,174 @@
-#include<stdio.h>
-#include<string.h>
-
-int main()
+#include <stdio.h>
+#include <string.h>
+void revrse(char str[])
 {
-    char str1[100],str2[100];
-    int choice,length,i,rep,num,sum=0,rem,reverce=0,temp;
-    start:
-    printf("\nPress 1 for reverse a string");
-    printf("\nress 2 for Concatenation");
-    printf("\nPress 3 for Palindrome");
-    printf("\nPress 4 for Copy a string ");
-    printf("\nPress 5 for Length of the string");
-    printf("\nPress 6 for Frequency of character in s string ");
-    printf("\nPress 7 for Check the number of vowels and consonants ");
-    printf("\nPress 8 for Check the number of blank spaces and digits ");
-
-    printf("\nPlease Enter your choice = ");
-    scanf("%d",&choice);
-
-    switch(choice)
-    {
-    
-        case 1:
-            printf("\nEnter the value of str = ");
-	        scanf("%s",str1);
-
-            printf("\noriginal value of str1 = %s",str1);
-            
-            strrev(str1);
-	        printf("\nReversed  string = %s",str1);
-        break;
-        case 2:
-            printf("\nEnter the value of str1 = ");
-	        scanf("%s",str1);
-            printf("\nEnter the value of str2 = ");
-	        scanf("%s",str2);
-
-            printf("\noriginal value of str1 = %s",str1);
-            printf("\noriginal value of str1 = %s",str2);
-
-            strcat(str1,str2);
-            printf("\nConcated string = %s",str1);
-        break;
-        case 3:
-
-            printf("\nEnter the value of num = ");
-            scanf("%d",&num);
-
-            temp = num;
-
-            while (num!=0)
-            {
-                rem = num%10;
-                reverce = (reverce*10) + rem;
-                num = num/10;
+	int i;
+	int len = strlen(str);
+            printf("Reversed string: ");
+            for (i = len - 1; i >= 0; i--)
+			 {
+                printf("%c", str[i]);
             }
-
-            printf("\nReverce = %d",reverce);
-
-            if (reverce==temp)
-            {
-                printf("\n%d is Palindrome number",reverce);
+            printf("\n");           
+}
+void concatenate( char str[],char str2[])
+{
+	strcat(str, str2);
+            printf("Concatenated string= %s\n", str);
+}
+int palindrome(char str[])
+{
+	char rev[100];
+	strcpy(rev,str);
+	strrev(rev);
+           int palindrome=strcmp(rev,str);
+           return palindrome;
+}	
+void copy(char str2[], char str[])
+{
+	strcpy(str2, str);
+              printf("Value of str after copy=%s\n",str);
+              printf("Value of str2 after copy=%s\n",str2);
+}
+int length(char str[])
+{
+			int len=strlen(str);
+			return len;
+}
+void Frequency(char str[])
+{
+	char ch;
+            int count = 0,i;
+            printf("Enter a character to find its frequency=");
+            scanf(" %c", &ch);
+            for (i = 0; str[i] != '\0'; i++) 
+			{
+                if (str[i] == ch)
+                {
+                	count++;
+				}
             }
-            else
-            {
-                printf("\n%d is not Palindrome number",reverce);
+            printf("Frequency of %c=%d\n",ch,count);
+}
+void countvowelconsonants(char str[])
+{
+	int vowels = 0, consonants = 0,i;
+            for (i = 0; str[i] != '\0'; i++) {
+                char ch = tolower(str[i]);
+                if (ch>='a'&&ch<='z') {
+                    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                        vowels++;
+                    else
+                        consonants++;
+                }
             }
-        break;
-        case 4:
-            printf("\nEnter the value of str1 = ");
-	        scanf("%s",str1);
+            printf("Vowels: %d, Consonants: %d\n", vowels, consonants);
+}
+void countspacesanddigits(char str[])
+{
+	
+            int spaces = 0, digits = 0,i;
+            for (i = 0; str[i] != '\0'; i++) {
+                if (isspace(str[i]))
+                    spaces++;
+                if (isdigit(str[i]))
+                    digits++;
+            }
+            printf("Spaces: %d, Digits: %d\n", spaces-1, digits);
+}
+int main() {
+ char str[100];
+ int i,j,choice, cont;
 
-            printf("\noriginal value of str1 = %s",str1);
-            printf("\noriginal value of str2 = %s",str2);
+    printf("Enter a string=");
+    fgets(str,50,stdin);
 
-            strcat(str2,str1);
-            printf("\nCopied string = %s",str1);
-            printf("\nCopied string = %s",str2);
-        break;
+start:
+    printf("\nEnter your choice :\n");
+    printf("press 1. for Reverse the string\n");
+    printf("press 2. for Concatenate \n");
+    printf("press 3. for Check for Palindrome\n");
+    printf("press 4. for Copy the string\n");
+    printf("press 5. for Length of the string\n");
+    printf("press 6. for Frequency of a character\n");
+    printf("press 7. for Count vowels and consonants\n");
+    printf("press 8. for Count spaces and digits\n");
+
+    printf("Please Enter your choice : ");
+    scanf("%d", &choice);
+
+    switch (choice) 
+	{
+        case 1: 
+		{
+		revrse(str);
+            break;
+        }
+        case 2: 
+		{
+            char str2[50];
+            printf("Enter another string to concatenate= ");
+            getchar();
+            fgets(str2,50, stdin);
+            concatenate(str,str2);
+            break;
+        }
+        case 3: 
+		{
+         int result= palindrome(str);
+         if(result==0)
+            {
+            	printf("This is a palindrome");
+			}
+			else
+			{
+				printf("This is not a palindrome");
+			}
+            break;
+        }
+        case 4: 
+		{
+            char str2[50];
+            printf("Enter another string=");
+            getchar();
+            fgets(str2,50, stdin);
+            printf("orignal value of str=%s\n",str);
+             printf("orignal value of str2=%s\n",str2);
+             copy(str2,str);
+             
+            break;
+        }
         case 5:
-            printf("\nEnter the value of str = ");
-	        scanf("%s",str1);
-
-            printf("\noriginal value of str1 = %s",str1);
-
-            length = strlen(str1);
-	        printf("\nLennth of  string = %d",length);
-        break;
-        case 6:
-            printf("\nEnter the value of str = ");
-	        scanf("%s",str1);
-
-            printf("\noriginal value of str1 = %s",str1);
-
-            length = strlen(str1);
-	        printf("\nLennth of  string = %d",length);
-        break;
+		 {
+            int result=length(str);
+            printf("The length of string is=%d",result-1);
+            break;
+        }
+        case 6: 
+		{
+            Frequency(str);
+            break;
+        }
         case 7:
-            printf("\nEnter the value of str = ");
-	        scanf("%s",str1);
-            for(i=0;str1[i]!='\0';i++)
-            {
-                if(i>='a' && i<='z' || i>='A' && i<='Z')
-                {
-                    length++;
-                }
-            }
-	        printf("\nTotal Vowel or consonant = %d",length);
-        break;
-        case 8:
-            printf("\nEnter the value of str = ");
-	        scanf("%s",str1);
-            for(i=0;str1[i]!='\0';i++)
-            {
-                if(i=' ' || i >= '0' && i<= '9')
-                {
-                    length++;
-                }
-            }
-	        printf("\nTotal blank space and digits are = %d",length);
-        break;
+		 {
+            countvowelconsonants(str);
+            break;
+        }
+        case 8: 
+		{        
+            countspacesanddigits(str);
+            break;
+        }
+        default:
+            printf("Invalid choice. No operation performed.\n");
     }
-    printf("\nDo you want to continue<press 1 to  continue>");
-    printf("\nEnter your choice = ");
-    scanf("%d",&rep);
-
-    if(rep == 1)
-    {
+    printf("\nDo you want to continue? Enter 1 for yes= ");
+    scanf("%d", &cont);
+    if (cont == 1) 
+	{
         goto start;
     }
 
-
+    
     return 0;
 }
